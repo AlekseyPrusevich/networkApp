8ec08d183439a67603dd111054f8727a0402aa6c
Aleksey <44395857+AlekseyPrusevich@users.noreply.github.com>
2020-08-15 16:27:18 +0300

    Bild #0.2

---
+++ networkApp/Controllers/LecturesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace networkApp.Controllers
+{
+    public class LecturesController : Controller
+    {
+        public IActionResult Motherboard()
+        {
+            return View();
+        }
+    }
+}
